e4293ffd65321fd5584514342290d3ad07c7766e
sumeet <sumeetshir@gmail.com>
2020-11-08 18:06:00 +0530

    added in two

--- two.java
+++ two.java
@@ -1,6 +1,14 @@
 class two{
     public static void main(String[] args) {
         system.out.println("this is second code...@@@");
+        switch (choice) {
+            case a:
+                system.out.println("adding the number ")
+                break;
         
+            default:
+            system.out.println("invalid ")
+                break;
+        }
     }
 }
